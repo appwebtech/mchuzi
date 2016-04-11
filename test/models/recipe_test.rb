3c247f15e8ed1baea5f15093334569e898e8120e
theappwebtech <josembi@gmail.com>
2016-04-11 21:27:33 +0000

    created forms and partials.

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -4,7 +4,9 @@ class RecipeTest < ActiveSupport::TestCase
     
     def setup
         @chef = Chef.create(chefname: "bob", email: "bob@example.com")
-        @recipe = @chef.recipes.build(name: "chicken parm", summary: "this is the best chicken parm recipe ever", description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
+        @recipe = @chef.recipes.build(name: "chicken parm", 
+        summary: "this is the best chicken parm recipe ever", 
+        description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
     end
     
     test "recipe should be valid" do
